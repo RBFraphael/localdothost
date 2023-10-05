3431c357c31db41f5b3c8d3fe0b61f8326bd3596
RBFraphael <rbfraphael@gmail.com>
2023-10-04 22:06:34 -0300

    Implemented in-app updates

--- modules/gui_src/main/libs/localhost-js.js
+++ modules/gui_src/main/libs/localhost-js.js
@@ -1,32 +1,168 @@
-import { ipcMain } from "electron"
+import { app, ipcMain } from "electron"
 import EventEmitter from "events";
 import { join } from "path";
 import fs from "fs";
+import axios from "axios";
+import child_process from "child_process";
+
+const githubToken = `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`;
 
 var modulesDir = process.env.NODE_ENV === "production" ?
               join(__dirname, "../../../../") : 
               join(__dirname, "../../");
-
+var updatePackagePath = join(modulesDir, "../update.exe");
 var handler = new EventEmitter();
+var updateCheckStatus = new EventEmitter();
+var updateProcess = null;
+var versions = null;
 
 const getThirdPartyAppsVersions = () => {
     let versionsFilePath = join(modulesDir, "versions.json");
     fs.readFile(versionsFilePath, (err, data) => {
         if(!err){
-            let versions = JSON.parse(data);
-            handler.emit("versions-load", versions);
+            versions = JSON.parse(data);
+            handler.emit("versions-load");
         }
     });
 };
+
+const checkForUpdates = () => {
+    if(fs.existsSync(updatePackagePath)){
+        fs.unlinkSync(updatePackagePath);
+    }
+    
+    if(versions == null){
+        getThirdPartyAppsVersions();
+    } else {
+        updateCheckStatus.emit("checking");
+
+        axios.get("https://api.github.com/repos/rbfraphael/localdothost/releases", {
+            headers: {
+                'Accept': "application/json",
+                'Authorization': `Bearer ${githubToken}`
+            }
+        }).then((res) => {
+            let latestRelease = res.data[0];
+            
+            let currentVersion = parseInt(versions.gui.replace(/\D/g, ""));
+            let latestVersion = parseInt(latestRelease.name.replace(/\D/g, ""));
+
+            if(latestVersion > currentVersion){
+                updateCheckStatus.emit("available", latestRelease.name);
+            } else {
+                updateCheckStatus.emit("updated");
+            }
+        }).catch((err) => {
+            updateCheckStatus.emit("error");
+        });
+    }
+}
+
+const downloadLatestRelease = async () => {
+    updateCheckStatus.emit("checking");
+
+    axios.get("https://api.github.com/repos/rbfraphael/localdothost/releases", {
+        headers: {
+            'Accept': "application/json",
+            'Authorization': `Bearer ${githubToken}`
+        }
+    }).then((res) => {
+        let latestRelease = res.data[0];
+        let hasUpdatePackage = false;
+
+        latestRelease.assets.forEach((asset) => {
+            if(asset.name == "update.exe"){
+                hasUpdatePackage = true;
+                updateCheckStatus.emit("downloading");
+
+                let updateFile = fs.createWriteStream(updatePackagePath);
+
+                axios({
+                    method: "GET",
+                    url: asset.browser_download_url,
+                    responseType: "stream"
+                }).then((res) => {
+                    return new Promise((resolve, reject) => {
+                        res.data.pipe(updateFile);
+
+                        let error = null;
+
+                        updateFile.on('error', err => {
+                            updateCheckStatus.emit("error");
+                            error = err;
+                            updateFile.close();
+                            reject(err);
+                        });
+
+                        updateFile.on('close', () => {
+                            if (!error) {
+                                updateCheckStatus.emit("ready");
+                                resolve(true);
+                            }
+                        });
+                    });
+                });
+            }
+        });
+
+        if(!hasUpdatePackage){
+            updateCheckStatus.emit("error");
+        }
+    }).catch((err) => {
+        updateCheckStatus.emit("error");
+    });
+}
+
+const applyUpdate = () => {
+    updateProcess = child_process.spawn(updatePackagePath, {
+        detached: true
+    });
+
+    updateProcess.on("spawn", () => {
+        app.quit();
+    });
+}
 
 export const declareLocalHostIpcEvents = () => {
     ipcMain.on("localhost-versions", (e) => {
         getThirdPartyAppsVersions();
     });
+
+    ipcMain.on("check-for-updates", (e) => {
+        checkForUpdates();
+    });
+
+    ipcMain.on("download-latest-release", (e) => {
+        downloadLatestRelease();
+    });
+
+    ipcMain.on("apply-update", (e) => {
+        applyUpdate();
+    });
 };
 
 export const declareLocalHostCallbackEvents = (window) => {
-    handler.on("versions-load", (versions) => {
+    handler.on("versions-load", () => {
         window.webContents.send("localhost-versions-load", versions);
+        checkForUpdates();
+    });
+
+    updateCheckStatus.on("checking", () => {
+        window.webContents.send("update-check-status", "checking");
+    });
+    updateCheckStatus.on("available", (newVersion) => {
+        window.webContents.send("update-check-status", "available", newVersion);
+    });
+    updateCheckStatus.on("updated", () => {
+        window.webContents.send("update-check-status", "updated");
+    });
+    updateCheckStatus.on("error", () => {
+        window.webContents.send("update-check-status", "error");
+    });
+    updateCheckStatus.on("downloading", () => {
+        window.webContents.send("update-check-status", "downloading");
+    });
+    updateCheckStatus.on("ready", () => {
+        window.webContents.send("update-check-status", "ready");
     });
 };
--- modules/gui_src/renderer/components/AppAbout.jsx
+++ modules/gui_src/renderer/components/AppAbout.jsx
@@ -1,5 +1,5 @@
 import electron from "electron";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, CircularProgress, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Typography } from "@mui/material";
 import Image from "next/image";
 import Logo from "../assets/logo.png";
 import { useEffect, useState } from "react";
@@ -9,6 +9,14 @@ const ipcRenderer = electron.ipcRenderer || false;
 export default function AppAbout()
 {
     const [versions, setVersions] = useState({});
+    const [latestVersion, setLatestVersion] = useState("");
+    const [updateCheckStatus, setUpdateCheckStatus] = useState("");
+    const [userUpdateCheck, setUserUpdateCheck] = useState(false);
+    const [isDownloadingUpdate, setIsDownloadingUpdate] = useState(false);
+
+    const [showDialog, setShowDialog] = useState(false);
+    const [dialogText, setDialogText] = useState("");
+    const [showUpdateDialog, setShowUpdateDialog] = useState(false);
 
     useEffect(() => {
         ipcRenderer.send("localhost-versions");
@@ -16,60 +24,162 @@ export default function AppAbout()
         ipcRenderer.on("localhost-versions-load", (e, data) => {
             setVersions(data);
         });
+
+        ipcRenderer.on("update-check-status", (e, status, newVersion = null) => {
+            setUpdateCheckStatus(status);
+
+            if(status == "available"){
+                setLatestVersion(newVersion);
+            }
+        });
+
     }, []);
 
-    return (
-        <Box sx={{ width: "100%" }}>
-            <Typography variant="h5" sx={{ marginBottom: "1rem" }}>Local.Host About</Typography>
+    useEffect(() => {
+        if(updateCheckStatus == "updated"){
+            if(userUpdateCheck == true){
+                setDialogText("You have the latest available version.");
+                setShowDialog(true);
+            };
+        }
+        
+        if(updateCheckStatus == "available"){
+            if(userUpdateCheck){
+                setDialogText(`There's a new version available to download: ${latestVersion}`);
+            } else {
+                setDialogText(`There's a new version available to download: ${latestVersion}. Go to About tab to download and install this update.`);
+            }
+            setShowDialog(true);
+        }
 
-            <Box sx={{ width: "100%", marginBottom: "1rem" }}>
-                
-                <Box sx={{ width: "100%", display: "flex", flexDirection: "row" }}>
-                    <Box sx={{ marginRight: "3rem" }}>
-                        <Image src={Logo} width={150} height={150} style={{ marginLeft: "auto", marginRight: "auto", marginBottom: "1rem" }} />
-                    </Box>
-                    <Box sx={{ marginRight: "3rem" }}>
-                        <Typography variant="body1">
-                            <strong>Developed by:</strong> RBFraphael &lt;rbfraphael@gmail.com&gt;
-                        </Typography>
-                        <Typography variant="body1">
-                            <strong>Github URL:</strong> https://github.com/rbfraphael
-                        </Typography>
-                        <Typography variant="body1">
-                            <strong>GUI Version:</strong> { versions.gui ?? "" }
-                        </Typography>
-                        <Typography variant="body1">
-                            <strong>Apache:</strong> { versions.apache ?? "" }
-                        </Typography>
-                        <Typography variant="body1">
-                            <strong>PHP:</strong> { versions.php ? Object.values(versions.php).join(" / ") : "" }
-                        </Typography>
-                        <Typography variant="body1">
-                            <strong>Composer:</strong> { versions.composer ?? "" }
-                        </Typography>
-                    </Box>
-                    <Box>
-                        <Typography variant="body1">
-                            <strong>MariaDB:</strong> { versions.mariadb ?? "" }
-                        </Typography>
-                        <Typography variant="body1">
-                            <strong>HeidiSQL:</strong> { versions.heidisql ?? "" }
-                        </Typography>
-                        <Typography variant="body1">
-                            <strong>Acrylic DNS:</strong> { versions.acrylic ?? "" }
-                        </Typography>
-                        <Typography variant="body1">
-                            <strong>MongoDB:</strong> { versions.mongodb ?? "" }
-                        </Typography>
-                        <Typography variant="body1">
-                            <strong>MongoDB Compass:</strong> { versions.compass ?? "" }
-                        </Typography>
-                        <Typography variant="body1">
-                            <strong>NVM:</strong> { versions.nvm ?? "" }
-                        </Typography>
+        if(updateCheckStatus == "ready"){
+            setDialogText(`The update package for version ${latestVersion} was downloaded. Before installing the update, we recommend you to stop all running services.`);
+            setShowDialog(true);
+        }
+
+        if(updateCheckStatus == "error"){
+            if(userUpdateCheck == true || isDownloadingUpdate){
+                setDialogText("An error has occurred when gathering update information. Try again later.");
+                setShowDialog(true);
+            };
+        }
+    }, [updateCheckStatus]);
+
+    const checkForUpdates = () => {
+        setUserUpdateCheck(true);
+        ipcRenderer.send("check-for-updates");
+    };
+
+    const downloadLatestVersion = () => {
+        setIsDownloadingUpdate(true);
+        ipcRenderer.send("download-latest-release");
+    }
+
+    const installUpdate = () => {
+        setShowUpdateDialog(false);
+        ipcRenderer.send("apply-update");
+    }
+
+    const updateDialog = () => {
+        setShowUpdateDialog(true);
+    };
+
+    const closeDialog = () => {
+        setShowDialog(false);
+    };
+
+    return (
+        <>
+            <Box sx={{ width: "100%" }}>
+                <Typography variant="h5" sx={{ marginBottom: "1rem" }}>Local.Host About</Typography>
+
+                <Box sx={{ width: "100%", marginBottom: "1rem" }}>
+                    
+                    <Box sx={{ width: "100%", display: "flex", flexDirection: "row" }}>
+                        <Box sx={{ marginRight: "3rem" }}>
+                            <Image src={Logo} width={150} height={150} style={{ marginLeft: "auto", marginRight: "auto", marginBottom: "1rem" }} />
+                        </Box>
+                        <Box sx={{ marginRight: "3rem" }}>
+                            <Typography variant="body1">
+                                <strong>Developed by:</strong> RBFraphael &lt;rbfraphael@gmail.com&gt;
+                            </Typography>
+                            <Typography variant="body1">
+                                <strong>Github URL:</strong> https://github.com/rbfraphael
+                            </Typography>
+                            <Typography variant="body1">
+                                <strong>GUI Version:</strong> { versions.gui ?? "" }
+                            </Typography>
+                            <Typography variant="body1">
+                                <strong>Apache:</strong> { versions.apache ?? "" }
+                            </Typography>
+                            <Typography variant="body1">
+                                <strong>PHP:</strong> { versions.php ? Object.values(versions.php).join(" / ") : "" }
+                            </Typography>
+                            <Typography variant="body1">
+                                <strong>Composer:</strong> { versions.composer ?? "" }
+                            </Typography>
+                        </Box>
+                        <Box>
+                            <Typography variant="body1">
+                                <strong>MariaDB:</strong> { versions.mariadb ?? "" }
+                            </Typography>
+                            <Typography variant="body1">
+                                <strong>HeidiSQL:</strong> { versions.heidisql ?? "" }
+                            </Typography>
+                            <Typography variant="body1">
+                                <strong>Acrylic DNS:</strong> { versions.acrylic ?? "" }
+                            </Typography>
+                            <Typography variant="body1">
+                                <strong>MongoDB:</strong> { versions.mongodb ?? "" }
+                            </Typography>
+                            <Typography variant="body1">
+                                <strong>MongoDB Compass:</strong> { versions.compass ?? "" }
+                            </Typography>
+                            <Typography variant="body1">
+                                <strong>NVM:</strong> { versions.nvm ?? "" }
+                            </Typography>
+                        </Box>
                     </Box>
                 </Box>
+
+                <Box sx={{ width: "100%", marginBottom: "1rem" }}>
+                    { updateCheckStatus == "checking" && (
+                        <CircularProgress />
+                    ) }
+                    { updateCheckStatus == "available" && (
+                        <Button variant="contained" onClick={() => downloadLatestVersion()}>Download version { latestVersion }</Button>
+                    ) }
+                    { (updateCheckStatus == "" || updateCheckStatus == "updated" || updateCheckStatus == "error") && (
+                        <Button variant="contained" onClick={() => checkForUpdates()}>Check for updates</Button>
+                    ) }
+                    { updateCheckStatus == "downloading" && (
+                        <CircularProgress color="secondary" />
+                    ) }
+                    { updateCheckStatus == "ready" && (
+                        <Button variant="contained" onClick={() => updateDialog()}>Install version { latestVersion }</Button>
+                    ) }
+                </Box>
             </Box>
-        </Box>
+
+            <Dialog open={showDialog} onClose={closeDialog}>
+                <DialogTitle></DialogTitle>
+                <DialogContent>
+                    <DialogContentText>{dialogText}</DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={closeDialog}>Ok</Button>
+                </DialogActions>
+            </Dialog>
+
+            <Dialog open={showUpdateDialog} onClose={installUpdate}>
+                <DialogTitle></DialogTitle>
+                <DialogContent>
+                    <DialogContentText>Before installing the update, we recommend you to stop all running services. Also, if you have made some modifications on one or more files of Local.Host's modules, you need to backup then before applying the update, and re-applying your modified files after the update.</DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={installUpdate}>Ok, install {latestVersion}</Button>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 }
