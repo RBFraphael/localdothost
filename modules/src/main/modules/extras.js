17c83e2d662317bbbdabfe8e94aa51809e571946
RBFraphael <rbfraphael@gmail.com>
2023-12-20 01:52:47 -0300

    Added CLI PHP version changing on GUI

--- modules/src/main/modules/extras.js
+++ modules/src/main/modules/extras.js
@@ -3,12 +3,24 @@ const path = require("path");
 const { getModulesDir } = require("../helpers/paths");
 const { shell, ipcMain } = require("electron");
 const { getPath, setPath } = require("../helpers/envvars");
+const { readFileSync, existsSync, writeFileSync } = require("fs");
 
 const binDir = path.join(getModulesDir(), "/bin");
 const heidiSqlDir = path.join(getModulesDir(), "/heidisql");
 const heidiSqlExe = path.join(heidiSqlDir, "heidisql.exe");
 
 const extrasStatus = new EventEmitter();
+
+const phpCli = path.join(binDir, "php.cmd");
+const phpVersions = {
+    '5.6': path.join(getModulesDir(), "/modules/php/5.6/php.exe"),
+    '7.0': path.join(getModulesDir(), "/modules/php/7.0/php.exe"),
+    '7.2': path.join(getModulesDir(), "/modules/php/7.2/php.exe"),
+    '7.4': path.join(getModulesDir(), "/modules/php/7.4/php.exe"),
+    '8.0': path.join(getModulesDir(), "/modules/php/8.0/php.exe"),
+    '8.2': path.join(getModulesDir(), "/modules/php/8.2/php.exe"),
+    '8.3': path.join(getModulesDir(), "/modules/php/8.3/php.exe")
+};
 
 const extras = (action) => {
     switch(action){
@@ -48,13 +60,49 @@ const openHeidiSql = () => {
     shell.openExternal(heidiSqlExe);
 }
 
+const getCliPhpVersion = () => {
+    if(existsSync(phpCli)){
+        let cliFile = readFileSync(phpCli).toString();
+        for(const [version, path] of Object.entries(phpVersions)){
+            if(cliFile.indexOf(version) > -1){
+                extrasStatus.emit("php-cli-version", version);
+            }
+        }
+    }
+};
+
+const setCliPhpVersion = (newVersion) => {
+    if(typeof newVersion == "number"){
+        newVersion = newVersion.toFixed(1).toString();
+    }
+
+    if(existsSync(phpCli)){
+        let cliFile = readFileSync(phpCli).toString();
+        for(const [version, path] of Object.entries(phpVersions)){
+            if(cliFile.indexOf(version) > -1){
+                cliFile = cliFile.replace(version, newVersion);
+                break;
+            }
+        }
+        writeFileSync(phpCli, cliFile);
+    }
+
+    getCliPhpVersion();
+};
+
 const init = (appWindow) => {
     ipcMain.on("extras", (e, action) => { extras(action); });
     ipcMain.on("extras-status", (e) => { getStatus(); });
     ipcMain.on("heidisql", (e) => { openHeidiSql(); });
+    ipcMain.on("php-cli", (e) => { getCliPhpVersion(); });
+    ipcMain.on("set-php-cli", (e, version) => { setCliPhpVersion(version); });
 
     extrasStatus.on("status", (status) => {
         appWindow.webContents.send("extras-status", status);
+    });
+
+    extrasStatus.on("php-cli-version", (version) => {
+        appWindow.webContents.send("php-cli-version", version);
     });
 };
 
--- modules/src/src/components/Extras.tsx
+++ modules/src/src/components/Extras.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, CircularProgress, Typography } from "@mui/material";
+import { Box, Button, CircularProgress, FormControl, InputLabel, MenuItem, Select, Typography } from "@mui/material";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import ComposerLogo from "../assets/composer.png";
@@ -7,9 +7,17 @@ import PhpLogo from "../assets/php.png";
 export default function Extras()
 {
     const [status, setStatus] = useState("uninstalled");
+    const [currentCliPhpVersion, setCurrentCliPhpVersion] = useState("");
+
+    const phpVersions = ["5.6", "7.0", "7.2", "7.4", "8.0", "8.2", "8.3"];
 
     useEffect(() => {
         window.ipcRenderer.send("extras-status");
+        window.ipcRenderer.send("php-cli");
+
+        window.ipcRenderer.on("php-cli-version", (e: any, version: string) => {
+            setCurrentCliPhpVersion(version);
+        });
 
         window.ipcRenderer.on("extras-status", (e: any, status: string) => {
             setStatus(status);
@@ -20,13 +28,17 @@ export default function Extras()
         window.ipcRenderer.send("extras", action);
     };
 
+    const onChangeCliPhpVersion = (version: string) => {
+        window.ipcRenderer.send("set-php-cli", version);
+    };
+
     return (
         <>
             <Box sx={{ width: "100%" }}>
                 <Typography variant="h5" sx={{ marginBottom: "1rem" }}>Commandline Tools</Typography>
 
                 <Box sx={{ display: "flex", gap: ".5rem" }}>
-                    <Box>
+                    <Box sx={{ flexBasis: "50%" }}>
                         <Typography variant="h6" sx={{ marginBottom: "0.5rem" }}>PHP and Composer</Typography>
                         
                         <Box sx={{width: "100%", marginBottom: "1rem"}}>
@@ -47,6 +59,16 @@ export default function Extras()
                         { status == "installed" && (
                             <Button onClick={() => extras("uninstall")} variant="contained" color="error">Uninstall</Button>
                         )}
+                    </Box>
+                    <Box sx={{ flexBasis: "30%" }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="php-cli-version-label">PHP CLI version</InputLabel>
+                            <Select labelId="php-cli-version-label" id="php-cli-version" value={currentCliPhpVersion} label="Active version" onChange={(e) => onChangeCliPhpVersion(e.target.value)}>
+                                { phpVersions.map((version, index) => (
+                                    <MenuItem key={index} value={version}>{version}</MenuItem>
+                                )) }
+                            </Select>
+                        </FormControl>
                     </Box>
                 </Box>
             </Box>
